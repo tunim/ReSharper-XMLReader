4677c84088d5bb83a7eec41fd8281a3915fc2fbd
tunim <tunim.7y@gmail.com>
2018-04-13 13:56:39 -0300

    #1 - add upload feature

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Xml;
 using System.Xml.Linq;
+
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+
 using WebApp.Models;
 
 namespace WebApp.Controllers
@@ -18,6 +19,8 @@ namespace WebApp.Controllers
         private ICollection<IssueType> _issueTypes;
         private ICollection<Issue> _issues;
 
+        private readonly string _destinationPath;
+
         public HomeController()
         {
             _categories = new List<IssueType.CategoryType>();
@@ -25,30 +28,48 @@ namespace WebApp.Controllers
             _issueTypes = new List<IssueType>();
             _issues = new List<Issue>();
 
+            _destinationPath = Path.Combine(Directory.GetCurrentDirectory(), "UploadedFiles", "resharper-results.xml");
         }
 
         public IActionResult Index(string[] categoryTypes = null, string[] severities = null, string[] issueTypes = null, string[] fileTypes = null)
         {
-            LoadXml();
+            if (ValidateXmlReport())
+            {
+                LoadXml();
 
-            ViewBag.IssueTypes = new SelectList(_issueTypes, "Id", "Description");
-            ViewBag.FileTypes = new SelectList(_fileTypes);
-            ViewBag.CategoryTypes = new SelectList(_categories, "Id", "Description");
-            ViewBag.Severities = new SelectList(_issueTypes.Select(x => x.Severity).Distinct().ToList());
+                ViewBag.IssueTypes = new SelectList(_issueTypes, "Id", "Description");
+                ViewBag.FileTypes = new SelectList(_fileTypes);
+                ViewBag.CategoryTypes = new SelectList(_categories, "Id", "Description");
+                ViewBag.Severities = new SelectList(_issueTypes.Select(x => x.Severity).Distinct().ToList());
 
-            var issues = FilterIssues(categoryTypes, severities, issueTypes, fileTypes);
+                var issues = FilterIssues(categoryTypes, severities, issueTypes, fileTypes);
 
-            return View(issues);
+                return View(issues);
+            }
+
+            return View();
         }
 
-        private void LoadXml()
+        [HttpPost]
+        public async Task<IActionResult> UploadAsync(IFormFile xml)
         {
-            var xmlPath = @"C:\ResharperResults\r-results.xml";
-            var xEle = XElement.Load(xmlPath);
+            if (xml != null && xml.ContentType == "text/xml")
+            {
+                using (var stream = new FileStream(_destinationPath, FileMode.Create))
+                    await xml.CopyToAsync(stream);
 
-            _issueTypes = LoadIssueTypes(xEle);
-            _issues = LoadIssues(xEle);
-            
+                if (ValidateXmlReport())
+                    return RedirectToAction("Index");
+                else
+                {
+                    System.IO.File.Delete(_destinationPath);
+                    ModelState.AddModelError("xml", "The file loaded is not a valid ReSharper report.");
+                }
+            }
+            else
+                ModelState.AddModelError("xml", "Send a valid xml report");
+
+            return View("Index");
         }
 
         private ICollection<Issue> FilterIssues(string[] categoryTypes, string[] severities, string[] issueTypes, string[] fileTypes)
@@ -68,6 +89,63 @@ namespace WebApp.Controllers
                 issues = issues.Where(x => fileTypes.Contains(x.FileType));
 
             return issues.ToList();
+        }
+
+        private bool ValidateXmlReport()
+        {
+            if (!System.IO.File.Exists(_destinationPath))
+                return false;
+
+            var xEle = XElement.Load(_destinationPath);
+            var validXml = xEle != null && xEle.Element("IssueTypes") != null && xEle.Element("Issues") != null;
+
+            var issueTypesValid = validXml && xEle.Element("IssueTypes").HasElements 
+                && xEle.Element("IssueTypes").Elements().Any(x => x.HasAttributes
+                    && x.Attribute("Id") != null
+                    && x.Attribute("Description") != null
+                    && x.Attribute("Severity") != null
+                    && x.Attribute("CategoryId") != null
+                    && x.Attribute("Category") != null);
+            
+            var issuesValid = validXml && xEle.Element("Issues").Elements().Any(x => x.HasAttributes
+                && x.Attribute("Name") != null
+                && x.Elements()
+                    .Any(y => y.Name == "Issue"
+                        && y.HasAttributes
+                        && y.Attribute("TypeId") != null
+                        && y.Attribute("File") != null
+                        && y.Attribute("Message") != null));
+
+            return issueTypesValid && issuesValid;
+        }
+
+        private void LoadXml()
+        {
+            var xEle = XElement.Load(_destinationPath);
+
+            _issueTypes = LoadIssueTypes(xEle);
+            _issues = LoadIssues(xEle);
+            
+        }
+
+        private ICollection<IssueType> LoadIssueTypes(XElement xElement)
+        {
+            var issues = new List<IssueType>();
+            foreach (var xEle in xElement.Element("IssueTypes").Elements())
+            {
+                var issueType = new IssueType
+                {
+                    Id = xEle.Attribute("Id").Value,
+                    Description = xEle.Attribute("Description").Value,
+                    Severity = xEle.Attribute("Severity").Value,
+                    WikiUrl = xEle.Attribute("WikiUrl")?.Value,
+                    Category = GetCategory(xEle)
+                };
+
+                issues.Add(issueType);
+            }
+
+            return issues;
         }
         
         private ICollection<Issue> LoadIssues(XElement xElement)
@@ -114,26 +192,6 @@ namespace WebApp.Controllers
         private IssueType GetIssueType(string issueTypeId)
         {
             return _issueTypes.FirstOrDefault(x => x.Id == issueTypeId);
-        }
-
-        private ICollection<IssueType> LoadIssueTypes(XElement xElement)
-        {
-            var issues = new List<IssueType>();
-            foreach (var xEle in xElement.Element("IssueTypes").Elements())
-            {
-                var issueType = new IssueType
-                {
-                    Id = xEle.Attribute("Id").Value,
-                    Description = xEle.Attribute("Description").Value,
-                    Severity = xEle.Attribute("Severity").Value,
-                    WikiUrl = xEle.Attribute("WikiUrl")?.Value,
-                    Category = GetCategory(xEle)
-                };
-
-                issues.Add(issueType);
-            }
-
-            return issues;
         }
 
         private IssueType.CategoryType GetCategory(XElement xElement)
